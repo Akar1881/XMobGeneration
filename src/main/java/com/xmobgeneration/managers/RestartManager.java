ac35883ba23361de0267e48f02a557b5a09b437e
Akar1881 <yahsirroque35nj@gmail.com>
2024-12-29 00:38:25 -0800

    adding restart interval in config.yml

--- src/main/java/com/xmobgeneration/managers/RestartManager.java
+++ src/main/java/com/xmobgeneration/managers/RestartManager.java
@@ -8,23 +8,42 @@ import org.bukkit.scheduler.BukkitTask;
 public class RestartManager {
     private final XMobGeneration plugin;
     private BukkitTask restartTask;
-    private static final long RESTART_INTERVAL = 1275L; // 10 minutes in ticks (20 ticks = 1 second)
     private static final long WARNING_TIME = 1200L; // 1 minute in ticks
+    private static final long DEFAULT_RESTART_INTERVAL = 12000L; // 10 minutes in ticks
+    private static final long MIN_RESTART_INTERVAL = 1200L; // 1 minute in ticks
 
     public RestartManager(XMobGeneration plugin) {
         this.plugin = plugin;
         startRestartTask();
     }
 
+    private long getRestartInterval() {
+        int configInterval = plugin.getConfigManager().getConfig().getInt("settings.restart-interval", 10);
+        
+        // Convert minutes to ticks (20 ticks = 1 second)
+        long intervalTicks = configInterval * 60L * 20L;
+        
+        // Ensure interval is greater than 1 minute
+        if (intervalTicks <= MIN_RESTART_INTERVAL) {
+            plugin.getLogger().warning("Restart interval must be greater than 1 minute! Using default value of 10 minutes.");
+            Bukkit.broadcastMessage(plugin.getConfigManager().getMessage("invalid-restart-interval"));
+            return DEFAULT_RESTART_INTERVAL;
+        }
+        
+        return intervalTicks;
+    }
+
     private void startRestartTask() {
+        final long restartInterval = getRestartInterval();
+        
         restartTask = new BukkitRunnable() {
-            private long ticksUntilRestart = RESTART_INTERVAL;
+            private long ticksUntilRestart = restartInterval;
 
             @Override
             public void run() {
                 if (ticksUntilRestart <= 0) {
                     restartMobAreas();
-                    ticksUntilRestart = RESTART_INTERVAL;
+                    ticksUntilRestart = restartInterval;
                 } else if (ticksUntilRestart == WARNING_TIME) {
                     Bukkit.broadcastMessage(plugin.getConfigManager().getMessage("restart-warning")
                         .replace("%minutes%", "1"));
